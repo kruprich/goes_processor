baf59f84023deeaa05b7ec678d9c3631595d571b
Kevin Ruprich <kevinruprich@MacBook-Air-2.local>
2024-12-30 21:10:15 -0500

    testing and moving

---
+++ goes_processor/config.py
@@ -0,0 +1,7 @@
+RETRIES = 3
+MAX_DOWNLOAD_CONCURRENCY = 64
+MAX_PROCESS_CONCURRENCY = 16
+TIMEOUT_SECONDS = 30
+LAT_BOUNDS = [0, 55]
+LON_BOUNDS = [-135, -45]
+FIELDS = ['flash_id', 'flash_time_offset_of_first_event', 'product_time', 'flash_lat', 'flash_lon', 'flash_quality_flag', 'flash_energy']
